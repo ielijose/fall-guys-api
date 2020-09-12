c15383c47e10d02124ac16f679e2cca34eb77130
Eli José Carrasquero <ielijose@gmail.com>
2020-09-12 12:20:33 -0300

    Group by type

--- src/pages/api/skins/index.js
+++ src/pages/api/skins/index.js
@@ -6,7 +6,16 @@ export default async function handler(req, res) {
 
   try {
     const skins = await Skin.find({})
-    res.status(200).json(skins)
+    const skinsMap = skins.reduce((prev, curr) => {
+      if (!prev[curr.type]) {
+        prev[curr.type] = []
+      }
+      return {
+        ...prev,
+        [curr.type]: [...prev[curr.type], curr],
+      }
+    }, {})
+    res.status(200).json(skinsMap)
   } catch (error) {
     res.status(500).json({ error: true, message: error.message })
   }
